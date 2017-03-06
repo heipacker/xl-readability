59e0af24837014d2fe8679552bc8411a5c48b6cf
付盼 <fupan@xunlei.com>
2017-03-06 22:20:00 +0800

    new: 添加默认userAgent

--- src/getContent.js
+++ src/getContent.js
@@ -2,6 +2,7 @@ var page = require('webpage').create();
 var args = require('system').args;
 var url = args[1];
 
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 page.open(url, function () {
     page.evaluate(function(){
 
