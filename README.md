26d3b83b921b08d3028e670088558691769abe32
付盼 <fupan@xunlei.com>
2017-05-02 18:18:30 +0800

    new: 修改模块名，　发布

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 * 是在readability开源项目的基础上增加了phantoms对动态网页的支持。
 
 
-# 发布ycc-node-readability：
+# 发布xunlei-readability：
 * https://www.npmjs.com 网址上面注册用户
 * 将工程打包copy至服务器，在根目录下面修改package.json中的版本号
 ```
-"name": "ycc-node-readability",
+"name": "xunlei-readability",
   "version": "1.0.0",
   "author": "lihaiou",
   "description": "Turning any web page into a clean view.",
@@ -19,5 +19,5 @@
 修改version
 * 执行npm adduser,输入在https://www.npmjs.com  上面的用户名，密码和邮箱
 * 执行npm publish
-* 需要使用的地方执行: npm install ycc-node-readability
+* 需要使用的地方执行: npm install ycc-readability
 * 如果在安装phantomjs经常按不上， 可以手动安装, 然后将phantomjs放到PATH中即可
--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,6 @@
 var read = require('../src/readability');
 
-read('http://colorlines.com/archives/2011/08/dispatch_from_angola_faith-based_slavery_in_a_louisiana_prison.html',
+read('https://mp.weixin.qq.com/s?__biz=MTQzMjE1NjQwMQ==&mid=2655539011&idx=3&sn=649070570126d38a6c8cc56c018855d2&chksm=66dfe0dd51a869cb0c086dfa3c0d95a723f1b860824a7f4b09cf84fc959a20982db0c509862d&mpshare=1&scene=1&srcid=0314oZn4FVCiYzUUQkaixKCH&pass_ticket=WQCQF01fbzmwzQlyWvBGdSRImd5MNgbP%2Fen4Ec2XLd%2BKV2tessA1VukgrjA9eZ63#rd',
 function(err, article, meta) {
   var dom = article.document;
   var html = '<html><head><meta charset="utf-8"><title>'+dom.title+'</title></head><body><h1>'+article.title+'</h1>'+article.content+'</body></html>';
